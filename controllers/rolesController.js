232f5e6ca75492eb5f4e6e7738810960bdbbbff5
faruqneo <faruq@marmeto.com>
2019-02-14 12:59:18 +0530

    roles module is completed some validation message are only left

--- controllers/rolesController.js
+++ controllers/rolesController.js
@@ -1,6 +1,6 @@
 const Roles = require('../model/role')
 
-
+//adding new roles
 exports.addNew = (req, res) => {
     req.checkBody('title', 'Title is required').notEmpty();
     req.checkBody('description', 'Description is required').notEmpty();
@@ -26,13 +26,14 @@ exports.addNew = (req, res) => {
             }
             else
             {
-                req.flash('success', 'Role Added') // not working
+                req.flash('success', 'Role Added')
                 res.redirect('/cms/dashboard/roles')
             }
         });
     }
 }
 
+//list view for roles
 exports.rolesList = (req, res) => {
     Roles.find({},function(err, roles){
         if(err)
@@ -45,4 +46,61 @@ exports.rolesList = (req, res) => {
             });
         }
     });
+}
+
+//detailes view for role
+exports.rolesView = (req, res) => {
+    Roles.findById(req.params.id,function(err, roles){
+            res.render('rolesView', {
+                roles
+            });
+        })
+}
+
+//update for roles
+exports.rolesUpdate = (req, res) => {
+    req.checkBody('title', 'Title is required').notEmpty();
+    req.checkBody('description', 'Description is required').notEmpty();
+
+    let errors = req.validationErrors();
+    
+    if(errors)
+    {
+        res.render('rolesView',{
+            errors
+        }); 
+    }
+    else
+    {
+        let roles = req.body;
+        let id = {_id:req.params.id}
+        //console.log(req.params.id)
+        Roles.updateOne(id, roles,function(err){
+            if(err)
+            {
+                console.log(err)
+            }
+            else
+            {
+                req.flash('success', 'Role Update')
+                res.redirect('/cms/dashboard/roles')
+            }
+        })
+    }
+}
+
+//role delete 
+exports.rolesDelete = (req, res) => {
+    let id = {_id:req.params.id}
+    Roles.deleteOne(id, function(err){
+        if(err)
+        {
+            console.log(err)
+        }
+        else
+        {
+            req.flash('success', 'Role Deleted')
+            res.redirect('/cms/dashboard/roles')
+        }
+    })
 }
--- router/cms.js
+++ router/cms.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const router = express.Router()
 const authController = require('../controllers/authController')
 const rolesController = require('../controllers/rolesController')
+const router = express.Router()
 
 router.get('/', (req, res) => {
     res.render('home',{layout: false})
@@ -29,16 +29,17 @@ router.get('/dashboard/password/add', authController.ensureAthenticated, (req, r
     res.render('add_passwords')
 })
 
+//roles routers
 router.get('/dashboard/roles', authController.ensureAthenticated, rolesController.rolesList)
-
 router.get('/dashboard/roles/add', authController.ensureAthenticated, (req, res) => {
     res.render('add_roles')
 })
-
 router.post('/dashboard/roles/new_add', authController.ensureAthenticated, rolesController.addNew)
+router.get('/dashboard/roles/edit/:id', authController.ensureAthenticated, rolesController.rolesView)
+router.post('/dashboard/update/roles/:id', authController.ensureAthenticated, rolesController.rolesUpdate)
+router.get('/dashboard/roles/delete/:id', authController.ensureAthenticated, rolesController.rolesDelete)
 
-// router.post('/dashboard/roles/edit/:id', authController.ensureAthenticated, )
-
+//logout routes
 router.get('/logout', authController.ensureAthenticated, authController.logout)
 
 
